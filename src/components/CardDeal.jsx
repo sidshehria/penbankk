b16ca68686cccb7dbab95ea9e3bce6a39053041a
Siddharth Shehria <83392744+sidshehria@users.noreply.github.com>
2024-05-29 23:05:06 +0530

    Add files via upload

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Find the best card deals quickly and easily! Our simple, step-by-step process helps you compare top offers and discover the perfect card for your needs. 
+      Save time and money with our intuitive platform today!
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
